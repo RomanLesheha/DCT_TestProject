65cfbf8d6f1924a3541b115ebb21c6d559dacfe7
RomanLesheha <roman.leshega2003@gmail.com>
2023-05-18 19:00:54 +0300

    I created a chart on the main page that shows a list of the TOP15 cryptocurrencies .I am starting to create a new chart on which the price change schedule for a certain time period of a certain currency will be placed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Runtime.Serialization;
 
 namespace DCT_TestProject
 {
@@ -24,16 +26,35 @@ namespace DCT_TestProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        static public SeriesCollection SeriesCollection { get; set; }
+        static public string[] Labels { get; set; }
+        static public Func<double, string> YFormatter { get; set; }
+
+        private void Chart_Click(object sender, ChartPoint chartPoint)
+        {
+            // Retrieve the index of the clicked data point
+            var index = (int)chartPoint.X;
+
+            // Retrieve the corresponding name from the Labels array
+            var clickedName = Labels[index];
+
+            // Display the name (you can use MessageBox or any other UI element)
+            MessageBox.Show($"Clicked Name: {clickedName}");
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             Parse();
         }
-        
+       
+       
+
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             Window1 window = new Window1();
             window.Show();
+            
         }
         private async void Parse()
         {
@@ -57,12 +78,24 @@ namespace DCT_TestProject
             foreach (var item in sortedData)
             {
                 dtCurrency.Rows.Add(item.name, item.priceUsd);
+
             }
+            SeriesCollection = new SeriesCollection
+                {
+                    new ColumnSeries
+                    {
+                        Title = "Price (USD)",
+                        Values = new ChartValues<double>(dtCurrency.Rows.OfType<DataRow>().Select(row => Convert.ToDouble(row["Price (USD)"])))
+                    }
+                };
 
-            datagrid1.ItemsSource = dtCurrency.DefaultView;
+            Labels = dtCurrency.Rows.OfType<DataRow>().Select(row => row["Name"].ToString()).ToArray();
 
-
+            YFormatter = value => value.ToString("C");
+            DataContext = this;
         }
+
+
     }
     public class Asset
     {
